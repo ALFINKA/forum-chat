6e2fa77513e27bbf3564bce00e67999f36f8fe11
saufiroja <saufiroja08@gmail.com>
2022-12-07 20:34:08 +0700

    feat: init server

--- backend/auth/app/server/fiber.go
+++ backend/auth/app/server/fiber.go
@@ -1,1 +1,17 @@
 package server
+
+import (
+	"github.com/gofiber/fiber/v2"
+)
+
+func Server() *fiber.App {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{
+			"message": "API is running",
+		})
+	})
+
+	return app
+}
--- backend/auth/main.go
+++ backend/auth/main.go
@@ -1,1 +1,23 @@
 package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/alfinka/backend/auth/app/server"
+)
+
+func main() {
+	app := server.Server()
+
+	err := app.Listen(":8080")
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt, syscall.SIGTERM)
+	<-quit
+}
